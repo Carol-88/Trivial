3b7197484b535b962f15de84e7a2c51231fdf8e7
Carol-88 <gralleta@gmail.com>
2022-11-16 10:46:55 +0100

    Añado las respuestas a los botones, creado el botón hid con la respuesta correcta, prueba de css, no va.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 "use strict";
 const titleQ = document.querySelector("p");
-const answer1 = document.querySelector("option1");
-const answer2 = document.querySelector("option2");
-const answer3 = document.querySelector("option3");
-const answer4 = document.querySelector("option4");
-const buttonNext = document.querySelector("next-button");
+const answer1 = document.querySelector(".option1");
+const answer2 = document.querySelector(".option2");
+const answer3 = document.querySelector(".option3");
+const answer4 = document.querySelector(".option4");
+const answCorrect = document.querySelector(".hidecorrect");
+const buttonNext = document.querySelector("#next-button");
 
 async function quiz() {
   const res = await fetch("json/quiz.json");
@@ -13,7 +14,15 @@ async function quiz() {
 
   console.log(body);
 
+  //Podriamos hacer un if, para que cuando toque en la respuesta correcta, sume 1 a score, y sino, else, no sume nada.
+  //Y se podría hacer un for para que vaya cambiando el 0 de body, hasta 50, cada vez que le de al boton de next
+
   titleQ.append(body[0].question);
+  answer1.append(body[0].answers[0]);
+  answer2.append(body[0].answers[1]);
+  answer3.append(body[0].answers[2]);
+  answer4.append(body[0].answers[3]);
+  answCorrect.append(body[0].correct);
 }
 
 quiz();
