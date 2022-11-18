c64b4c2a36d1d06679048a17ef7df42c69db4b27
Carol-88 <gralleta@gmail.com>
2022-11-18 12:57:25 +0100

    Añado try catch a la funcion asincrona

--- js/main.js
+++ js/main.js
@@ -7,32 +7,36 @@ const body = document.querySelector("body");
 let index = 0;
 
 async function startQuiz() {
-  const res = await fetch("json/quiz.json");
+  try {
+    const res = await fetch("json/quiz.json");
+    const quiz = await res.json();
 
-  const quiz = await res.json();
-
-  function generateRound() {
-    titleQ.textContent = quiz[index].question;
-    for (let buttonIndex = 0; buttonIndex < buttons.length; buttonIndex++) {
-      buttons[buttonIndex].textContent = quiz[index].answers[buttonIndex];
+    function generateRound() {
+      titleQ.textContent = quiz[index].question;
+      for (let buttonIndex = 0; buttonIndex < buttons.length; buttonIndex++) {
+        buttons[buttonIndex].textContent = quiz[index].answers[buttonIndex];
+      }
     }
+
+    generateRound();
+  } catch (error) {
+    console.log(error.message);
   }
-  generateRound();
-
-  section.addEventListener("click", (event) => {
-    if (event.target.className !== "buttOptions") {
-      return;
-    }
-    if (event.target.textContent === quiz[index].correct) {
-      scoreCount.textContent++;
-    }
-    if (index < quiz.length - 1) {
-      index++;
-      generateRound();
-    } else {
-      body.innerHTML = `<h1>¡¡Tu puntuación es ${scoreCount.textContent}!!</h1>`;
-    }
-  });
 }
+
+section.addEventListener("click", (event) => {
+  if (event.target.className !== "buttOptions") {
+    return;
+  }
+  if (event.target.textContent === quiz[index].correct) {
+    scoreCount.textContent++;
+  }
+  if (index < quiz.length - 1) {
+    index++;
+    generateRound();
+  } else {
+    body.innerHTML = `<h1>¡¡Tu puntuación es ${scoreCount.textContent}!!</h1>`;
+  }
+});
 
 startQuiz();
